f9505efae166dde9e1e90da6e12150a4ec3ad83f
adamnfish <adamnfish@gmail.com>
2021-12-04 15:24:25 +0000

    Add static site

---
+++ docs/precache-manifest.9f4305ef8ff0f9f2b471a5efcc39baf8.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "987da976a8b2a4169346a497ba84749d",
+    "url": "/lines/favicon.ico"
+  },
+  {
+    "revision": "3023d66531c926df03a45255f848adc1",
+    "url": "/lines/index.html"
+  },
+  {
+    "revision": "6ead519905b7b907c00965be37dd5cd7",
+    "url": "/lines/logo.svg"
+  },
+  {
+    "revision": "1c82903c2994c2507b385b9042bbfac5",
+    "url": "/lines/manifest.json"
+  },
+  {
+    "revision": "121e17251c55d8b8d12f",
+    "url": "/lines/static/css/main.0af5c45f.chunk.css"
+  },
+  {
+    "revision": "121e17251c55d8b8d12f",
+    "url": "/lines/static/js/main.121e1725.chunk.js"
+  },
+  {
+    "revision": "dc8935bfb8afc11834a2",
+    "url": "/lines/static/js/runtime~main.dc8935bf.js"
+  },
+  {
+    "revision": "0b49ec49a892799f59f3",
+    "url": "/lines/static/js/vendors~main.0b49ec49.chunk.js"
+  }
+]);
